258a8ebecbe6891442292246b64c65eb72f2c7ed
sirius-fan <hi.fanjialiang@gmail.com>
2025-01-17 15:10:19 +0800

    nvovide map config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -22,3 +22,38 @@ vim.api.nvim_create_user_command('Q', 'qall', {
 
 -- map("n", "<esc>[1;3B", "ddP", { desc = "Move line down" })
 -- map("n", "<C-S-Down>", "ddp", { desc = "Move line down" })
+
+-- neovide 实现ctrl+shift+v 和 ctrl+shift+c 和 shift+insert
+
+if vim.g.neovide then
+	-- map('v', '<C-S-c>', '"+y', { noremap = true, silent = true })
+	map('v', '<C-S-v>', '"+p', { noremap = true, silent = true })
+	map("v", "<sc-c>", '"+y', { noremap = true }) -- Select line(s) in visual mode and copy (CTRL+Shift+V)
+
+	map("n", "<sc-v>", '"+p', { noremap = true }) -- Paste in normal mode (CTRL+Shift+C)
+
+	map('i', '<S-Insert>', '<C-R>+', { noremap = true, silent = true })
+	map("i", "<sc-v>", '<ESC>"+p', { noremap = true }) -- Paste in insert mode (CTRL+Shift+C)
+
+	-- map('c', '<sc-v>', '<C-R>+', { noremap = true, silent = true }) -- 在命令模式下粘贴
+	map("c", "<sc-v>", "<C-R>+", { noremap = true }) -- 系统剪切板
+end
+
+
+-- macos
+if vim.fn.has("mac") then
+	if vim.g.neovide then
+		vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+		vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+		vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+		vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+		vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
+		vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+	end
+
+	-- Allow clipboard copy paste in neovim
+	vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+end
